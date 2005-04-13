5cd513952e9db65eddc274eea8d06a82adab0f48
pdm <pdm>
2005-04-13 18:50:47 +0000

    Revision: pdm@brailcom.org--pdm/voice-czech-ph--main--0.1--patch-4
    Author of the word list added

--- doc/recording/README.cs
+++ doc/recording/README.cs
@@ -1,9 +1,9 @@
 Tento adresář obsahuje fonetické podklady pro pořízení a zpracování difonové
 nahrávky.
 
-Soubor `words' obsahuje seznam slov k nahrání spolu s difony, které
-reprezentují.  Jde o soubor připravený Fonetickým ústavem Karlovy univerzity.
-Pokyny pro jeho interpretaci mluvčím jsou v souboru README.speaker.cs.
+Soubor `words' obsahuje seznam slov s difony, jehož původním autorem je
+Mgr. Pavel Machač z Fonetického ústavu Univerzity Karlovy.  Tento seznam slov
+je určen k namluvení mluvčím dle pokynů uvedených v souboru README.speaker.cs.
 
 Soubor `phonemes' obsahuje seznam všech použitých fonetických jednotek
 s popisem.
